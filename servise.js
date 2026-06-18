e181fd3cc8d87439c716c534252b425b1cbce7fe
guddu kumar <guddu.edugaon@gmail.com>
2026-06-18 15:17:09 +0530

    wishlist api me pot no. change kiya gya hai

--- servise.js
+++ servise.js
@@ -12,8 +12,8 @@ app.use(
   wishlistRoutes
 );
 
-app.listen(3000, () => {
+app.listen(5000, () => {
   console.log(
-    "Server running on port 3000"
+    "Server running on port 5000"
   );
 });
